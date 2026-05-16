2dc35dd33a0f775862456d239083bce9beb6185d
Daniel Toplak <homer.simpson123@gmx.de>
2026-05-16 23:29:52 +0200

    fix: typecheck and lint

---
+++ apps/backend-e2e/src/support/global.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-var
+declare var __TEARDOWN_MESSAGE__: string;
--- apps/backend-e2e/src/support/test-setup.ts
+++ apps/backend-e2e/src/support/test-setup.ts
@@ -1,7 +1,6 @@
-/* eslint-disable */
 import axios from 'axios';
 
-module.exports = async function () {
+module.exports = function testSetup() {
   // Configure axios for tests to use.
   const host = process.env.HOST ?? 'localhost';
   const port = process.env.PORT ?? '3000';
