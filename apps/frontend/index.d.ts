ad50ba99b773a700a2f693272d3cd43223a6f97d
Daniel Toplak <homer.simpson123@gmx.de>
2026-05-28 00:15:30 +0200

    feat: cleanup

--- apps/frontend/index.d.ts
+++ apps/frontend/index.d.ts
@@ -1,4 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.css' {
+  const styles: Record<string, string>;
+  export default styles;
+}
+
 declare module '*.svg' {
   const content: any;
   export const ReactComponent: any;
