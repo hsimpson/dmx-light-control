19f532dbb39562059ebc440a0a46ce08ed7ec7bf
Daniel Toplak <homer.simpson123@gmx.de>
2026-08-30 14:33:53 +0200

    fix: UUIDv4 compatibility

---
+++ apps/backend/src/db/migrations/20260830143000_fix_invalid_channel_range_public_ids/migration.sql
@@ -0,0 +1,15 @@
+UPDATE "fixture_channel_ranges" SET "public_id" = '86d9355b-23e7-4ae3-ab82-f8b58114cbc9' WHERE "public_id" = 'a4b9e7c8-5f1d-4a2b-c9e4-7f3a8d1b5c2f';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = 'eae524fb-a9ee-4eec-b2d5-677f633838cc' WHERE "public_id" = 'b5c1f8d9-6a2e-4b3c-d1f5-8a4b9e2c6d3a';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = 'a9bf9045-251c-42c4-bb9b-a82cb3a560fb' WHERE "public_id" = 'c6d2a9e1-7b3f-4c4d-e2a6-9b5c1f3d7e4b';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = 'ce3bb491-71c3-4775-ba1f-f1b380964709' WHERE "public_id" = 'd7e3b1f2-8c4a-4d5e-f3b7-1c6d2a4e8f5c';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = 'fea72be9-3263-4250-a154-1786584e4a2b' WHERE "public_id" = 'a1b6e4c5-2f7d-4a8b-c6e1-4f9a5d7b2c8f';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '7f2518e6-b6d3-4f6e-b025-1219cf776bce' WHERE "public_id" = 'b2c7f5d6-3a8e-4b9c-d7f2-5a1b6e8c3d9a';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '01afa168-a588-4aa1-b830-c99cd4299247' WHERE "public_id" = 'c3d8a6e7-4b9f-4c1d-e8a3-6b2c7f9d4e1b';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '8113d452-1e64-439d-8bd5-5d0682ee644c' WHERE "public_id" = 'd4e9b7f8-5c1a-4d2e-f9b4-7c3d8a1e5f2c';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '8598ee3d-4766-42b2-977f-a9718ee5b787' WHERE "public_id" = 'a7b3e1c2-8f4d-4a5b-c3e7-1f6a2d4b8c5f';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '9896a005-fea3-4958-8e22-2bd9761f8202' WHERE "public_id" = 'b8c4f2d3-9a5e-4b6c-d4f8-2a7b3e5c9d6a';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '7bc668c5-a21e-4b30-ad56-167cbd12d91c' WHERE "public_id" = 'c9d5a3e4-1b6f-4c7d-e5a9-3b8c4f6d1e7b';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '77bf0c36-7c58-4663-ad21-41896c25f236' WHERE "public_id" = '3c4d5e6f-7a8b-4c9d-0e1f-2a3b4c5d6e7f';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '0acee898-dc9f-4ab0-8b7f-1f33589e16af' WHERE "public_id" = '4d5e6f7a-8b9c-4d0e-1f2a-3b4c5d6e7f8a';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = '95b56208-be95-4839-bc63-105f89c9831b' WHERE "public_id" = '5e6f7a8b-9c0d-4e1f-2a3b-4c5d6e7f8a9b';--> statement-breakpoint
+UPDATE "fixture_channel_ranges" SET "public_id" = 'd494e684-b697-4d27-ad1f-015f6c7cfee8' WHERE "public_id" = '6f7a8b9c-0d1e-4f2a-3b4c-5d6e7f8a9b0c';
